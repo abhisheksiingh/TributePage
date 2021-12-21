14ecba06b7d584f989b132dfbb9b57c8a7a152d1
abhisheksiingh <89836312+abhisheksiingh@users.noreply.github.com>
2021-12-21 21:42:58 +0530

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,6 @@
+
+
+/*Downloaded from https://www.codeseek.co/glgeorgiou/freecodecamp-my-tribute-page-womJzx */
+     .thumbnail {background-color: #eee;}
+    body {margin-top: 33px;}
+    .thumbnail .caption {background-color: #ddd;}
